5de19224d97344653acaffa0de214542512cfc17
Kenneth Yang <kenneth@gaiaplatform.io>
2021-07-12 14:07:03 -0700

    Implemented bot properties, used physics movement

--- WarehouseDemo/Scripts/Bots/BotMovementController.gd
+++ WarehouseDemo/Scripts/Bots/BotMovementController.gd
@@ -1,16 +1,28 @@
-extends Node
+extends KinematicBody2D
+
+#### Variables
+### Properties
+export (int) var payload_weight
+export (float) var max_speed
+export (float) var speed_scale = 500
+export (int) var batter_time
+export (int) var charge_time
+
+### Navigation
+var movement_path = []
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+func _physics_process(delta):
+	if movement_path.size() >= 2:  # There is still a goal coordinate to reach
+		if position.distance_squared_to(movement_path[1]) > 25:  # There is still distance to the next goal point
+			var dir = (movement_path[1] - movement_path[0]).normalized()  # Normalized direction of movement
+			var frame_speed_scale = speed_scale * delta
+			var vel_vector = (dir * frame_speed_scale).clamped(max_speed * frame_speed_scale)  # Speed scaled, clamped, and delta-ed movement vector
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+			var _bot_collision = move_and_collide(vel_vector)
+		else:
+			var _stop_movement = move_and_collide(Vector2.ZERO)  # Stop movement
+			movement_path.remove(0)
+	else:
+		if movement_path.size() && position != movement_path[0]:
+			position = movement_path[0]
--- WarehouseDemo/Scripts/Navigation/FactoryNav.gd
+++ WarehouseDemo/Scripts/Navigation/FactoryNav.gd
@@ -8,7 +8,6 @@ onready var bots = bots_node.get_children()
 
 ### Member variables
 export var location_index = 0
-export var bot_speed = 400
 
 const _factory_locations = [
 	Vector2(485, 496),  # WS Start
@@ -37,48 +36,32 @@ enum _factory_stop {
 	CHARGING_AREA_1
 }
 
-var _movement_path = []
 
 func _ready():
-	bots[0].position = _factory_locations[location_index]
-
-func _process(delta):
-	var walk_distance = bot_speed * delta
-	move_along_path(walk_distance)
+	bots[0].position = _factory_locations[_factory_stop.WS_START]
+	# bots[1].position = _factory_locations[_factory_stop.CHARGING_AREA_0]
+	# bots[2].position = _factory_locations[_factory_stop.BUFFER_AREA_1]
 
 
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
-		location_index+=1
+		location_index += 1
 		if location_index == _factory_locations.size():
 			location_index = 0
 
-		_update_navigation_path(bots[0].position, _factory_locations[location_index])
+		_update_navigation_path(bots[0], _factory_locations[location_index])
+
+		# var index_shift = 0
+		# for bot in bots:
+		# 	_update_navigation_path(bot, _factory_locations[location_index])
+
 
 # Navigation functions
-func move_along_path(distance):
-	var last_point = bots[0].position
-	while _movement_path.size():
-		var distance_between_points = last_point.distance_to(_movement_path[0])
-		# The position to move to falls between two points.
-		if distance <= distance_between_points:
-			bots[0].position = last_point.linear_interpolate(_movement_path[0], distance / distance_between_points)
-			return
-		# The position is past the end of the segment.
-		distance -= distance_between_points
-		last_point = _movement_path[0]
-		_movement_path.remove(0)
-	# The character reached the end of the path.
-	bots[0].position = last_point
-	set_process(false)
-
-
-func _update_navigation_path(start_position, end_position):
+func _update_navigation_path(bot, end_position):
 	# get_simple_path is part of the Navigation2D class.
 	# It returns a PoolVector2Array of points that lead you
 	# from the start_position to the end_position.
-	_movement_path = get_simple_path(start_position, end_position, true)
-	# The first point is always the start_position.
-	# We don't need it in this example as it corresponds to the character's position.
-	_movement_path.remove(0)
-	set_process(true)
+	var movement_path = get_simple_path(bot.position, end_position, true)
+
+	# Set bot's movement path
+	bot.movement_path = movement_path
