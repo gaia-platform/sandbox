33a1e2c0441237e9cb0e845e3e492a7c801143e3
Kenneth Yang <kenneth@gaiaplatform.io>
2021-07-22 18:45:54 -0700

    started porting logic

--- SandboxVisual/Scripts/AccessControl/UI/schedule_panel.gd
+++ SandboxVisual/Scripts/AccessControl/UI/schedule_panel.gd
@@ -1,6 +1,11 @@
 extends PanelContainer
 
 # Nodes
-export (PackedScene) var schedule_item
-export (NodePath) var schedule_list_path
-onready var schedule_list = get_node(schedule_list_path)
+export (PackedScene) var event_label
+export (NodePath) var event_list_path
+onready var event_list = get_node(event_list_path)
+
+
+# Methods
+func add_schedule_events(events: Dictionary, with_room_name: bool):
+	pass
