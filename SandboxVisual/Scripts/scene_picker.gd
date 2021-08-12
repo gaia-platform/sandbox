7d4e4a1acd16f8326538c94776ac81e48c76da25
Kenneth Yang <kenneth@gaiaplatform.io>
2021-08-12 13:57:23 -0700

    Object detection on circular machine

--- SandboxVisual/Scripts/scene_picker.gd
+++ SandboxVisual/Scripts/scene_picker.gd
@@ -2,6 +2,7 @@ extends Control
 
 export (String, FILE) var farobot_3_tasks_scene
 export (String, FILE) var amr_scenario_1_scene
+export (String, FILE) var sbdl_scene
 export (String, FILE) var access_control_scene
 
 
@@ -23,6 +24,14 @@ func _on_FARobot3TasksButton_pressed():
 
 func _on_AccessControlButton_pressed():
 	var change_scene_status = get_tree().change_scene(access_control_scene)
+	if change_scene_status != OK:
+		print("Error changing scene: %d" % change_scene_status)
+	else:
+		CommunicationManager.publish_project_action("select", "access_control_template")
+
+
+func _on_ScenarioBasedDataLogging_pressed():
+	var change_scene_status = get_tree().change_scene(sbdl_scene)
 	if change_scene_status != OK:
 		print("Error changing scene: %d" % change_scene_status)
 	else:
