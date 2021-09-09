c13a23237a868998dd60438930db9efdca3c5fef
Kenneth Yang <kenneth@gaiaplatform.io>
2021-09-08 17:59:23 -0700

    Add outbound pallet on reset Fixes #135

--- SandboxVisual/Scripts/Factory/Factories/amr_swarm_factory.gd
+++ SandboxVisual/Scripts/Factory/Factories/amr_swarm_factory.gd
@@ -228,6 +228,30 @@ func _on_ApplyButton_pressed():
 	receive_order_button.disabled = false
 	inbound_area.pallet_node = null
 
+	# Reset outbound area
+	var pallet_data = {"id": CommunicationManager.generate_uuid(), "widgets": []}
+	var outbound_pallet = pallet_scene.instance()
+	outbound_pallet.payload_id = pallet_data["id"]
+	outbound_pallet.global_position = outbound_area.pallet_location.get_location()
+	pallets.add_child(outbound_pallet)
+
+	# Move into place
+	outbound_area.add_pallet(outbound_pallet)
+
+	# For testing: Auto-populate it with 3 widgets
+	for w in 3:
+		var outbound_widget = widget_scene.instance()
+		outbound_widget.payload_id = CommunicationManager.generate_uuid()
+		pallet_data["widgets"].append({"id": outbound_widget.payload_id})
+		outbound_widget.global_position = outbound_pallet.global_position
+		widgets.add_child(outbound_widget)
+		outbound_pallet.add_widget(outbound_widget)
+
+	outbound_pallet.connect("widget_added", self, "_check_if_ready_to_ship")  # Connect to signal to check if ready to ship
+
+	# Tell Gaia a new pallet is in outbound
+	CommunicationManager.publish_to_app("station/outbound/pallet", to_json(pallet_data))
+
 	CommunicationManager.publish_to_app("ping", "running")
 
 
