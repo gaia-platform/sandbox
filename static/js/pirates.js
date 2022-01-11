a3058dcadd253f7fff72452f2141deb9b690e1f9
Keith Godfrey <keith@gaiaplatform.io>
2022-01-11 09:46:12 -0800

    clean up variable naming

--- static/js/pirates.js
+++ static/js/pirates.js
@@ -1,26 +1,26 @@
-var bg_img = new Image();
-var port_img = new Image();
-var merchant_img = new Image();
-var merchant_img_destroyed = new Image();
-var navy_img = new Image();
-var navy_img_attack = new Image();
-var pirate_img = new Image();
-var pirate_img_attack = new Image();
-var pirate_img_destroyed = new Image();
+var backgroundImage = new Image();
+var portImage = new Image();
+var merchantImage = new Image();
+var merchantDestroyedImage = new Image();
+var navyImage = new Image();
+var navyAttackImage = new Image();
+var pirateImage = new Image();
+var pirateAttackImage = new Image();
+var pirateDestroyedImage = new Image();
 
-// script for viewing multi-agent data
-// the switchboard is pinged every X msec to get the most recent
-//  position data and that is written to a canvas
+// Script for viewing multi-agent data
+// The pirate switchboard is pinged every X msec to get the most recent
+//  position data and that is written to a canvas.
 
 ////////////////////////////////////////////////////////////////////////
 // switchboard interface
 
 url = 'http://0.0.0.0:7111/get-positions/';
 
-// stores content returned from switchboard
+// Stores content returned from switchboard
 positionData = null;
 
-// hit switchboard's endpoint to get latest data
+// Hit switchboard's endpoint to get latest data
 function loadPositions() {
   fetch(url, { method: "GET", mode: 'cors', headers: {} })
     .then(response => response.json())
@@ -32,18 +32,18 @@ function loadPositions() {
 // main script interface
 
 function init() {
-  bg_img.src = 'static/img/pirate/ocean.jpg';
-  port_img.src = 'static/img/pirate/port.png';
+  backgroundImage.src = 'static/img/pirate/ocean.jpg';
+  portImage.src = 'static/img/pirate/port.png';
   //
-  merchant_img.src = 'static/img/pirate/merchant.png';
-  merchant_img_destroyed.src = 'static/img/pirate/merchant-x.png';
+  merchantImage.src = 'static/img/pirate/merchant.png';
+  merchantDestroyedImage.src = 'static/img/pirate/merchant-x.png';
   //
-  navy_img.src = 'static/img/pirate/navy.png';
-  navy_img_attack.src = 'static/img/pirate/navy-attack.png';
+  navyImage.src = 'static/img/pirate/navy.png';
+  navyAttackImage.src = 'static/img/pirate/navy-attack.png';
   //
-  pirate_img.src = 'static/img/pirate/pirate.png';
-  pirate_img_attack.src = 'static/img/pirate/pirate-attack.png';
-  pirate_img_destroyed.src = 'static/img/pirate/pirate-x.png';
+  pirateImage.src = 'static/img/pirate/pirate.png';
+  pirateAttackImage.src = 'static/img/pirate/pirate-attack.png';
+  pirateDestroyedImage.src = 'static/img/pirate/pirate-x.png';
   //
   window.requestAnimationFrame(draw_bg);
 }
@@ -52,14 +52,12 @@ function init() {
 function draw_bg() {
   var ctx = document.getElementById('ocean-canvas').getContext('2d');
   ctx.globalCompositeOperation = 'destination-over';
-  console.log("Drawing water");
-  ctx.drawImage(bg_img, 0, 0);
+  ctx.drawImage(backgroundImage, 0, 0);
   draw_ports();
 }
 
 
 function draw_ports() {
-  console.log("Draw ports");
   if (positionData == null) {
     loadPositions();
     window.requestAnimationFrame(draw_ports, 1000);
@@ -82,7 +80,7 @@ function draw_ports() {
     //  as center (icon is 20x25)
     x -= 10;
     y -= 12;
-    ctx.drawImage(port_img, x, y);  
+    ctx.drawImage(portImage, x, y);  
   }
   draw_ships();
 }
@@ -91,7 +89,6 @@ function draw_ports() {
 function draw_ships() {
   var ctx = document.getElementById('ship-canvas').getContext('2d');
   ctx.globalCompositeOperation = 'destination-over';
-  console.log("Draw ships");
   ctx.clearRect(0, 0, 1350, 850); // clear canvas
   for (let i=10; i<70; i++) {
     vessel = positionData[i];
@@ -104,7 +101,6 @@ function draw_ships() {
       continue;
     }
     y = 0.001 * vessel['y'];
-    //console.log(vessel);
     // offset x and y for display. x,y is top-left of icon while we want it
     //  as center (icon is 20x20)
     x -= 10;
@@ -112,23 +108,23 @@ function draw_ships() {
     // determine ship type by ID
     if (i < 30) {   // merchant ?
       if (vessel['destroyed'] > 0) {
-        ctx.drawImage(merchant_img_destroyed, x, y);
+        ctx.drawImage(merchantDestroyedImage, x, y);
       } else {
-        ctx.drawImage(merchant_img, x, y);
+        ctx.drawImage(merchantImage, x, y);
       }
     } else if (i < 50) {    // navy?
       if (vessel['target-id'] >= 0) {
-        ctx.drawImage(navy_img_attack, x, y);
+        ctx.drawImage(navyAttackImage, x, y);
       } else {
-        ctx.drawImage(navy_img, x, y);
+        ctx.drawImage(navyImage, x, y);
       }
     } else if (i < 70) {    // pirate?
       if (vessel['destroyed'] > 0) {
-        ctx.drawImage(pirate_img_destroyed, x, y);
+        ctx.drawImage(pirateDestroyedImage, x, y);
       } else if (vessel['target-id'] >= 0) {
-        ctx.drawImage(pirate_img_attack, x, y);
+        ctx.drawImage(pirateAttackImage, x, y);
       } else {
-        ctx.drawImage(pirate_img, x, y);
+        ctx.drawImage(pirateImage, x, y);
       }
     }
   }
