5c24fb2389a3e3a84524429481777e45b366c8c1
Simone Rondelli <simone@gaiaplatform.io>
2022-03-14 10:38:02 +0100

    Address PR feedabck

--- static/js/tour.js
+++ static/js/tour.js
@@ -3,8 +3,8 @@ window.tour = new Tour({
         {
             element: "#ddl",
             title: "Define the Data Model",
-            content: "Defining the Data Model is the first step in creating a Gaia application. Gaia provides a SQL like Data Definition Language (DDL). " +
-                "You can define tables, fields and relationships between tables. Gaia generates C++ code to easily create/read/update/delete the data.",
+            content: "Defining the Data Model is the first step in creating a Gaia application. Gaia provides a SQL-like Data Definition Language (DDL). " +
+                "You can define tables, fields, and relationships between tables. Gaia generates C++ code to easily create/read/update/delete the data.",
             lineNumber: 12,
             tabId: 'string',
             backdrop: true,
@@ -15,7 +15,7 @@ window.tour = new Tour({
         {
             element: "#ruleset",
             title: "Define the Business Logic",
-            content: "Create business logic using rules. Rules are defined within a ruleset using the Gaia Declarative language which is a superset of C++." +
+            content: "Create business logic using rules. Rules are defined within a ruleset using the Gaia Declarative Language which is a superset of C++." +
                 "The Gaia Declarative Language allows to declaratively access your data, as defined in the DDL, and react to its changes.",
             backdrop: true,
             autoscroll: true,
@@ -24,8 +24,8 @@ window.tour = new Tour({
         },
         {
             element: "#ctrl-button",
-            title: "Build your application!",
-            content: "Builds the project running the gaia tools to convert the DDL and Ruleset into C++ source code.",
+            title: "Build your application",
+            content: "Builds the project running the Gaia tools to convert the DDL and Ruleset into C++ source code.",
             backdrop: true,
             autoscroll: true,
             smartPlacement: true,
@@ -34,7 +34,7 @@ window.tour = new Tour({
         {
             element: "#output",
             title: "Terminal",
-            content: "See the output of your program!",
+            content: "See the output of your program",
             backdrop: true,
             autoscroll: true,
             smartPlacement: true,
