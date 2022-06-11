c6958b65e59711b870209348e0890ccc373cae39
Laurentiu Cristofor <laurentiu@gaiaplatform.io>
2022-06-11 12:51:40 -0700

    Add MIT License and update copyright notices

--- agent/docker_build.sh
+++ agent/docker_build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#############################################
-# Copyright (c) Gaia Platform LLC
-# All rights reserved.
-#############################################
+###################################################
+# Copyright (c) Gaia Platform Authors
+#
+# Use of this source code is governed by the MIT
+# license that can be found in the LICENSE.txt file
+# or at https://opensource.org/licenses/MIT.
+###################################################
 
 # Simple function to start the process off.
 start_process() {
